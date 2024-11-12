f99fadf697dc28e3e41566f14b2f3ae4ab9d6f37
Paredesh26 <paredesh3418@student.faytechcc.edu>
2024-11-12 10:17:44 -0500

    1st HW of Intro to Python

---
+++ P1HW1_ParedesHaylee.py
@@ -0,0 +1,28 @@
+# Haylee Paredes
+# 09//19/24
+# P1HW1
+# Input and Ouput with mathematical expressions
+print("------Calculating Exponents------")
+print()
+print()
+# Get base value (as an integer) from the user
+base_value = int(input("Enter an integer as a base value: "))
+# Get exponent value from user
+exponent = int(input("Enter an integer as an exponent: "))
+# Raise the base_value to th exponent
+value = base_value ** exponent
+print()
+print()
+# Display output tp the user
+print(base_value, "raise to the power of", exponent, "is", value, "!!")
+print()
+print()
+print("------Addition and Subtraction------")
+print()
+print()
+start_num = int(input("Enter a starting integer: "))
+add_num = int(input("Enter an integer to add: "))
+sub_num = int(input("Enter an integer to subtract: "))
+print()
+print()
+print(start_num, "+", add_num, "-", sub_num, "is equal to", start_num + add_num - sub_num)
