a926d738697a30acfb95c402605e66abba58167a
Paredesh26 <paredesh3418@student.faytechcc.edu>
2024-11-12 16:33:35 +0000

    Added value-reeturning functions

--- Function_Example.py
+++ Function_Example.py
@@ -2,17 +2,34 @@
 
 # Defined a non-value rerturning funtion
 def my_animal(name, sound, pound_food):
-    print(f"The {name} make a {sound} noise!")
+    print(f"The {name} make a '{sound}' noise!")
     print(f"The {name} eats {pound_food} pound of food a day.")
-    print(f"The {name} eats {pound_food * 7} pound of food a week.")
-
+    print(f"The {name} eats {(pound_food * 7):.2f} pound of food a week.")
+# -----------------------------------------------------------------------
+def getName():
+    name = input("Enter your name: ")
+    return name + " *******"
+# -----------------------------------------------------------------------
+def displayName(first):
+    lastName = input("Enter your name: ")
+    fullName = first + " " + lastName
+    return fullName
+# -----------------------------------------------------------------------
 # Create a main function - all logic goes here
 def main():
     print("The main function is executing!")
     print()
     # Call the my_animal funtion
     my_animal("Lion", "rarwwwwr", 20.5)
-
+    print()
+    my_animal("Mouse", "ekekekekkk", 0.2)
+# -----------------------------------------------------------------------
+    # Call the value-returning function
+    myName = getName()
+    print(myName)
+    print()
+    print(displayName(myName))
+# -----------------------------------------------------------------------
 # Call the main function
 if __name__ == "__main__":
     main()
